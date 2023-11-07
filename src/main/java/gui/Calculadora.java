16a034d29d741d8ad06c0134dc5a1e17108e3905
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-11-06 20:15:33 -0500

    Update Calculadora.java

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -6,20 +6,20 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Calculadora extends Application  {
-	
-	public static void main(String args[]){ 
-	      ***(args); 
-	} 
-	
-	@Override
-	public void start(Stage window) throws Exception {
-		*** vb = new VBox();
-		window.setTitle(***);
-		
-		vb.getChildren().add(new Calculator());
-		Scene sc = new Scene(***, 270, 270);
-		window.setScene(***);
-		***.show();
-	}
-	
+
+    public static void main(String args[]){ 
+          launch(args); 
+    } 
+
+    @Override
+    public void start(Stage window) throws Exception {
+        VBox vb = new VBox();
+        window.setTitle("Calculadora");
+
+        vb.getChildren().add(new Calculator());
+        Scene sc = new Scene(vb, 270, 270);
+        window.setScene(sc);
+        window.show();
+    }
+
 }
